8c8a0ec58d5ae2736ec5294d2a660b29f192aa36
vishalsaik <saivishal003@gmail.com>
2026-03-20 12:46:25 +0530

    Vishal | App | extract connector bootstrap into app package with Initialize and Shutdown

---
+++ app/app.go
@@ -0,0 +1,69 @@
+package app
+
+import (
+	"context"
+
+	"api-monitoring/src/shared/config"
+	"api-monitoring/src/shared/config/logger"
+	"api-monitoring/src/shared/config/mongodb"
+	"api-monitoring/src/shared/config/postgres"
+	"api-monitoring/src/shared/config/rabbitmq"
+)
+
+type App struct {
+	Config   *config.Config
+	Log      *logger.Logger
+	Mongo    *mongodb.MongoDB
+	Postgres *postgres.Postgres
+	Rabbit   *rabbitmq.RabbitMQ
+}
+
+func Initialize() (*App, error) {
+	cfg := config.NewConfig()
+
+	log, err := logger.NewLogger(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	mongo, err := mongodb.NewMongoDBConfig(cfg, log)
+	if err != nil {
+		log.Error("Failed to connect to MongoDB")
+		return nil, err
+	}
+
+	pg, err := postgres.NewPostgres(cfg, log)
+	if err != nil {
+		log.Error("Failed to connect to Postgres")
+		return nil, err
+	}
+
+	rabbit, err := rabbitmq.NewRabbitMQ(cfg, log)
+	if err != nil {
+		log.Error("Failed to connect to RabbitMQ")
+		return nil, err
+	}
+
+	return &App{
+		Config:   cfg,
+		Log:      log,
+		Mongo:    mongo,
+		Postgres: pg,
+		Rabbit:   rabbit,
+	}, nil
+}
+
+func (a *App) Shutdown() {
+	if err := a.Mongo.Client.Disconnect(context.Background()); err != nil {
+		a.Log.Error("Failed to disconnect MongoDB")
+	} else {
+		a.Log.Info("Disconnected MongoDB successfully")
+	}
+
+	a.Postgres.Pool.Close()
+	a.Log.Info("Disconnected Postgres successfully")
+
+	a.Rabbit.Channel.Close()
+	a.Rabbit.Connection.Close()
+	a.Log.Info("Disconnected RabbitMQ successfully")
+}
